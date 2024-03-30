44be4a9f1f4da9cc0dcd9eceeb79d7f2ee47354c
Goutom Roy <goutomg1234@gmail.com>
2024-03-30 18:53:37 +0600

    For SingUP and Login .
    Singup,login,also forgotpass.and my CEO
    DashboardScene .

---
+++ Group-34 ( 5 Star Hotel ) Project-Final/src/ceo/HotelEmployeeController.java
@@ -0,0 +1,160 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ceo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import ModelClass.Employee;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author gouto
+ */
+public class HotelEmployeeController implements Initializable {
+
+    @FXML
+    private TableView<Employee> tableView;
+    @FXML
+    private TableColumn<Employee,Integer> idColum;
+    @FXML
+    private TableColumn<Employee,String> nameColum;
+    @FXML
+    private TableColumn<Employee,String> gerderColum;
+    @FXML
+    private TableColumn<Employee,LocalDate> dateofJoinColum;
+    @FXML
+    private TextField idtextField;
+    @FXML
+    private TextField nametextField;
+    @FXML
+    private ComboBox<String> comboBox;
+    @FXML
+    private DatePicker datePicker;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        comboBox.getItems().addAll("Male","Female","other");
+        
+       idColum.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("id"));
+       nameColum.setCellValueFactory(new PropertyValueFactory<Employee, String>("name"));
+       gerderColum.setCellValueFactory(new PropertyValueFactory<Employee, String>("gender"));
+       dateofJoinColum.setCellValueFactory(new PropertyValueFactory<Employee, LocalDate>("joining_date"));
+       
+       tableView.setItems(this.getEmployee());
+        tableView.setEditable(true);
+         
+       nameColum.setCellFactory(TextFieldTableCell.forTableColumn());
+       gerderColum.setCellFactory(TextFieldTableCell.forTableColumn());
+       
+       tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+       
+    }   
+    public ObservableList<Employee>getEmployee(){
+        
+        ObservableList<Employee> employee = FXCollections.observableArrayList();
+        
+        employee.add(new Employee(222034, "Goutom Roy", "Male", LocalDate.of(2002, Month.JULY, 13)));
+        employee.add(new Employee(222035, "Goutom Roy1", "Male", LocalDate.of(2003, Month.JUNE, 14)));
+        employee.add(new Employee(222036, "Goutom Roy2", "Male", LocalDate.of(2002, Month.JUNE, 16)));
+   return employee;
+   
+    }
+
+    @FXML
+    private void addnewEmployeeActionButton(ActionEvent event) {
+            try{
+            Employee u = new Employee(Integer.parseInt(idtextField.getText()),
+                    nametextField.getText(), comboBox.getValue(), datePicker.getValue());
+            
+            FileOutputStream fos = new FileOutputStream("C:/Users/gouto/Netbeans files1/Group Projects/Group-34 ( 5 Star Hotel ) Project/src/files/EmployeeDetailes.bin", true);
+            DataOutputStream dos = new DataOutputStream(fos);
+            
+            dos.writeInt(u.getId());
+            dos.writeUTF(u.getName());
+            dos.writeUTF(u.getGender());
+            LocalDate joiningDate = u.getJoining_date();
+             String dateString = joiningDate.toString();
+               dos.writeUTF(dateString);
+               
+//  --------------------------------------
+//read
+    FileInputStream fis = new FileInputStream("C:/Users/gouto/Netbeans files1/Group Projects/Group-34-(5-Star-Hotel)- Project-Final/Group-34--5-Star-Hotel--Project-Final/Group-34 ( 5 Star Hotel ) Project-Final/src/files/EmployeeDetailes.bin");
+    DataInputStream dis = new DataInputStream(fis);
+
+    ObservableList<Employee> employeeList = FXCollections.observableArrayList();
+
+    while (dis.available() > 0) {
+        int id = dis.readInt();
+        String name = dis.readUTF();
+        String gender = dis.readUTF();
+        LocalDate joiningDate1 = LocalDate.parse(dis.readUTF());
+
+        employeeList.add(new Employee(id, name, gender, joiningDate1));
+    }
+  tableView.setItems(employeeList);
+
+    dis.close();
+    fis.close();
+               
+               
+            
+        }catch (Exception e){
+                System.out.println(e);
+                
+        }
+    }
+
+    @FXML
+    private void EmployeeDetailsActionButton(ActionEvent event) throws IOException {
+        try {
+            
+    
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("EmployeeDetailsScene.fxml"));
+        Parent userViewParent = loader.load();
+        
+        Scene userViewScene = new Scene(userViewParent);
+        
+        EmployeeDetailsSceneController controller = loader.getController();
+        controller.initData(tableView.getSelectionModel().getSelectedItem());
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(userViewScene);
+        window.show();
+        
+            } catch (Exception e) {
+                System.out.println(e);
+        }
+    }
+    
+}
---
+++ Group-34 ( 5 Star Hotel ) Project-Final/src/it_manager/ForgotPasswordAndUpdatepassSceneController.java
@@ -0,0 +1,219 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package it_manager;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author gouto
+ */
+public class ForgotPasswordAndUpdatepassSceneController implements Initializable {
+
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private ComboBox<String> gendercomboBoxTextField;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private ComboBox<String> userComboBox;
+
+    @FXML
+    private AnchorPane emailAnchorpane;
+    @FXML
+    private AnchorPane updatepassAnchorPane;
+    @FXML
+    private PasswordField newPassWordTextField;
+    @FXML
+    private PasswordField confirmPassWordTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+             gendercomboBoxTextField.getItems().addAll("Male", "Female", "Other");
+        
+              userComboBox.getItems().addAll("CEO", "IT Manager", "Sales Manager", "Revenue Manager",
+                "Restaurant Manager", "Accountant", "Hotel Receptionist",
+                "Security Manager", "Customer", "Staff");
+        emailAnchorpane.setVisible(true);
+        updatepassAnchorPane.setVisible(false);
+    }    
+
+    @FXML
+    private void proceedOnActionButton(ActionEvent event) {
+            
+        String email = emailTextField.getText();
+        String gender=gendercomboBoxTextField.getValue().toString();
+        String dob = datePicker.getValue().toString();
+        String user=userComboBox.getValue().toString();
+        
+        boolean found = false;
+        String userEmail = "";
+        String usergender = "";       
+        String userDob = "";
+        String user1 = "";
+        try {
+             File file = new File("C:/Users/gouto/Netbeans files1/Group Projects/Group-34-(5-Star-Hotel)- Project-Final/Group-34--5-Star-Hotel--Project-Final/Group-34 ( 5 Star Hotel ) Project-Final/src/files/LoginUserDetails.txt");
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter("[;\n]");
+            
+                while (scanner.hasNext() && !found) {
+                userEmail = scanner.next();
+                scanner.next(); // Skip password field
+                user1=scanner.next();        // user role field
+                usergender=scanner.next(); //  gender field
+                userDob = scanner.next();  // date of birth
+
+                
+                if (email.equals(userEmail) &&  user.equals(user1)&& gender.equals(usergender)&& dob.equals(userDob)) {
+                    // Match found
+                    // Implement your logic to reset the password
+                    // For example, update the password in the user data file
+                    // Or send a temporary password to the user's email
+                    
+                    // In this example, we'll just display a message
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Password Reset");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Password reset successful!");
+                    alert.showAndWait();
+                    
+                    found = true;
+                }
+                }
+                
+                scanner.close();
+            
+            if (!found) {
+                // No matching email and date of birth found
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Email and/or Date of Birth do not match.");
+                alert.showAndWait();
+            }
+
+
+        emailAnchorpane.setVisible(false);
+        updatepassAnchorPane.setVisible(true);
+                
+        } catch (Exception e) {
+        }    
+    }
+
+    @FXML
+    private void updatepassOnAction(ActionEvent event) {
+        
+    String newPassword = newPassWordTextField.getText();
+    String confirmPassword = confirmPassWordTextField.getText();
+    
+       // Check if new password and confirmed password match
+
+       
+       try {
+        File file = new File("C:/Users/gouto/Netbeans files1/Group Projects/Group-34-(5-Star-Hotel)- Project-Final/Group-34--5-Star-Hotel--Project-Final/Group-34 ( 5 Star Hotel ) Project-Final/src/files/LoginUserDetails.txt");
+        File userInfoFile = new File("C:/Users/gouto/Netbeans files1/Group Projects/Group-34-(5-Star-Hotel)- Project-Final/Group-34--5-Star-Hotel--Project-Final/Group-34 ( 5 Star Hotel ) Project-Final/src/files/LoginUserInfo.txt");
+        File tempFile = new File("C:/Users/gouto/Netbeans files1/Group Projects/Group-34-(5-Star-Hotel)- Project-Final/Group-34--5-Star-Hotel--Project-Final/Group-34 ( 5 Star Hotel ) Project-Final/src/files/LoginUserDetails_temp.txt");
+         
+        Scanner userScanner = new Scanner(userInfoFile);
+         FileWriter userWriter = new FileWriter(userInfoFile);
+        Scanner scanner = new Scanner(file);
+        FileWriter writer = new FileWriter(tempFile);
+        
+            if (!newPassWordTextField.getText().equals(confirmPassWordTextField.getText())) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText("New password and confirmed password do not match.");
+        alert.showAndWait();
+        return;  
+            
+    }
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(";");
+            String email = parts[0];
+            String password = parts[1];
+            String role = parts[2];
+            String gender = parts[3];
+            String dob = parts[4];
+
+            // Update the password if the email matches
+            if (email.equals(emailTextField.getText()) && role.equals(userComboBox.getValue())) {
+                
+                password =  newPassword; // Update password
+            }
+
+            // Write the updated line to the temporary file
+            writer.write(email + ";" + password + ";" + role + ";" + gender + ";" + dob + "\n");
+            
+             // Write the updated line to the file
+            userWriter.write(email + ";" + password + ";" + role + "\n");
+        }
+
+        scanner.close();
+        writer.close();
+        
+        userScanner.close();
+        userWriter.close();
+        
+        // Delete the original file and rename the temporary file to the original filename
+        if (!file.delete()) {
+            System.out.println("Error deleting file");
+            return;
+        }
+        if (!tempFile.renameTo(file)) {
+            System.out.println("Error renaming file");
+            return;
+        }
+   
+        // Display a confirmation message
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Password Updated");
+        alert.setHeaderText(null);
+        alert.setContentText("Password updated successfully!");
+        alert.showAndWait();
+//-------------------------------------------------------------------
+        Parent Loginpage = FXMLLoader.load(getClass().getResource("/mainpkg/LoginScene.fxml"));
+        Scene scene = new Scene(Loginpage);
+        Stage addStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        addStage.setScene(scene);
+        addStage.show();
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+       
+    }
+    
+}
