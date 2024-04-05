a431e5d1323f8fd5322100df0ae1c033df4bd6d1
Goutom Roy <goutomg1234@gmail.com>
2024-04-05 16:16:41 +0600

    Add RestaurantManagerDeshBoard and AccountantDashBoard
    Add RestaurantManagerDeshBoard fxml,RestaurantManagerController,MenuManagementfxml,MenuManagementController,ViewSelectedItemDetails fxml,ViewSelectedItemDetailsController,AccountantDashboardfxml,AccountantDashboardController,ExpenditureScenefxml,ExpenditureSceneController,ExpenditureDetailsScenefxml,ExpenditureDetailsSceneController

---
+++ Group-34 ( 5 Star Hotel ) Project-Final/src/RestaurantManager/RestaurantManagerDeshBoardController.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package RestaurantManager;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class RestaurantManagerDeshBoardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void menuManagementButtonOnClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuManagement.fxml"));
+        Parent parent = loader.load();
+
+        
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+
+        Scene UserViewScene = new Scene(parent);
+
+        currentStage.setScene(UserViewScene);
+        currentStage.show(); 
+        
+    }
+
+    @FXML
+    private void orderManagementButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void feedbackButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void discountButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void backButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void logOutButtonOnClick(ActionEvent event) {
+    }
+    
+}
