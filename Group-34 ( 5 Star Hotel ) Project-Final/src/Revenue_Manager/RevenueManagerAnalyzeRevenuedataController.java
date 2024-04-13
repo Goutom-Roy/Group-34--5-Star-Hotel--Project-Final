41c19da99f082890a63998440da45dbe8a1a99ed
Ayesha <ayashaaktermeghla@gmail.com>
2024-04-13 20:02:00 +0600

    Commit

---
+++ Group-34 ( 5 Star Hotel ) Project-Final/src/Revenue_Manager/RevenueManagerAnalyzeRevenuedataController.java
@@ -0,0 +1,115 @@
+
+
+package RevenueManager;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+public class RevenueManagerAnalyzeRevenuedataController implements Initializable {
+
+    @FXML
+    private DatePicker StartDateField;
+    @FXML
+    private DatePicker EndDateField;
+    @FXML
+    private Label Analyzerevenuelabel;
+    @FXML
+    private Button Nextbuttonfxid;
+    @FXML
+    private ComboBox<String> ComboBox;
+    @FXML
+    private Label analyzeData;
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+         ComboBox.getItems().addAll("Room Revenue", "Food and Beverage Revenue", "Other Revenue");
+    
+    ComboBox.setValue("Room Revenue");
+        
+    }    
+
+    @FXML
+    private void Onclickanalyzebutton(ActionEvent event) {
+        
+        
+        
+        
+        
+    java.time.LocalDate startDate = StartDateField.getValue();
+    java.time.LocalDate endDate = EndDateField.getValue();
+    String selectedOption = ComboBox.getValue();
+
+    
+    
+  double revenue = 25000.0;
+  
+  String revenueAnalysis = "StartDate: "+ startDate +" to "+ " EndDate: "+ endDate  +" Category: "+ selectedOption +" , "+ " Total revenue is $" + String.valueOf(revenue);
+
+    analyzeData.setText(revenueAnalysis);
+        
+        
+        
+    try {
+        
+        FileWriter writer = new FileWriter("revenue_analysis.txt");
+        writer.write(revenueAnalysis);
+        writer.close();
+        
+    } catch (IOException e) {
+        e.printStackTrace();
+    } 
+        
+        
+        
+    }
+
+    @FXML
+    private void Onclicknextbutton(ActionEvent event) throws IOException {
+        
+        
+        
+          Stage stage = null;
+        Parent root = null;
+        
+           if(event.getSource()== Nextbuttonfxid){
+             
+             
+            stage = (Stage) Nextbuttonfxid.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("RevenueManagerManagediscounts.fxml"));
+        
+        
+        }
+     
+       
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Managediscounts");
+        stage.show();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ Group-34 ( 5 Star Hotel ) Project-Final/src/Revenue_Manager/RevenueManageranalyzeRevenueperformanceController.java
@@ -0,0 +1,93 @@
+
+package RevenueManager;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
+public class RevenueManageranalyzeRevenueperformanceController implements Initializable {
+
+    @FXML
+    private BarChart<String , Integer> Barchartrevenueperformance;
+    @FXML
+    private Button Nextbuttonfxid;
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+     
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Months");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Sales Performance");
+
+      
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>("January", 100));
+        series.getData().add(new XYChart.Data<>("February", 150));
+        series.getData().add(new XYChart.Data<>("March", 200));
+        series.getData().add(new XYChart.Data<>("April", 180));
+        series.getData().add(new XYChart.Data<>("May", 220));
+        series.getData().add(new XYChart.Data<>("June", 250));
+
+     
+        Barchartrevenueperformance.getData().add(series);
+
+      
+        Barchartrevenueperformance.setAnimated(false);
+        Barchartrevenueperformance.setCategoryGap(10);
+        Barchartrevenueperformance.setBarGap(5);
+        Barchartrevenueperformance.setHorizontalGridLinesVisible(true);
+        Barchartrevenueperformance.setVerticalGridLinesVisible(true);
+        Barchartrevenueperformance.setVerticalZeroLineVisible(true);
+        Barchartrevenueperformance.getXAxis().setLabel("Months");
+        Barchartrevenueperformance.getYAxis().setLabel("Revenue Performance");
+
+      
+        generateRandomData();
+    }
+
+   
+    private void generateRandomData() {
+        XYChart.Series<String, Integer> series = Barchartrevenueperformance.getData().get(0);
+        for (XYChart.Data<String, Integer> data : series.getData()) {
+         
+            int randomValue = (int) (Math.random() * 201) + 100;
+            data.setYValue(randomValue);
+        }
+    }
+        
+        
+
+    
+    @FXML
+    private void OnclickNextbutton(ActionEvent event) throws IOException {
+        
+        Stage stage = (Stage) Nextbuttonfxid.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("RevenueManagerHelpCenter.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("HelpCenter");
+        stage.show();
+        
+        
+        
+    }
+    
+}
